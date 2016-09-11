4a31e82c1e073f26cee99a823929f77d76c798a6
tuber <btu@thoughtworks.com>
2016-09-11 13:51:14 +0800

    [tuber] add more test case.

---
+++ src/main/java/ParkingLotBoy.java
@@ -0,0 +1,35 @@
+import java.util.List;
+import java.util.UUID;
+
+public class ParkingLotBoy {
+    private final List<ParkingLot> parkingLots;
+
+    public ParkingLotBoy(List<ParkingLot> parkingLots) {
+        this.parkingLots = parkingLots;
+    }
+
+    public UUID dropOff(Car car) throws NoAvailableLotException {
+        for (int i = 0; i < parkingLots.size(); ++i) {
+            ParkingLot parkingLot = parkingLots.get(i);
+            try {
+                return parkingLot.dropOff(car);
+            } catch (NoAvailableLotException e) {
+                if (i + 1 >= parkingLots.size()) {
+                    throw e;
+                }
+            }
+        }
+        return null;
+    }
+
+    public Car pickUp(UUID ticket) {
+        Car car = null;
+        for (ParkingLot parkingLot : parkingLots) {
+            car = parkingLot.pickUp(ticket);
+            if (car != null) {
+                break;
+            }
+        }
+        return car;
+    }
+}
---
+++ src/test/java/ParkingLotBoyTest.java
@@ -0,0 +1,72 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+public class ParkingLotBoyTest {
+    @Test
+    public void should_pick_the_origin_car_when_boy_park_a_car_success_with_one_parking_lot() throws NoAvailableLotException {
+        ParkingLot parkingLot = new ParkingLot(1);
+        List<ParkingLot> parkingLots = Arrays.asList(parkingLot);
+        ParkingLotBoy boy = new ParkingLotBoy(parkingLots);
+        Car myCar = new Car();
+
+        UUID myTicket = boy.dropOff(myCar);
+
+        assertThat(parkingLot.pickUp(myTicket), is(myCar));
+    }
+
+    @Test(expected = NoAvailableLotException.class)
+    public void should_throw_exception_when_boy_park_a_car_failure_with_one_parking_lot() throws NoAvailableLotException {
+        List<ParkingLot> parkingLots = Arrays.asList(new ParkingLot(0));
+        ParkingLotBoy boy = new ParkingLotBoy(parkingLots);
+        Car myCar = new Car();
+
+        boy.dropOff(myCar);
+    }
+
+    @Test
+    public void should_pick_the_origin_car_when_boy_park_a_car_success_with_two_parking_lots() throws NoAvailableLotException {
+        ParkingLot parkingLot = new ParkingLot(1);
+        List<ParkingLot> parkingLots = Arrays.asList(new ParkingLot(0), parkingLot);
+        ParkingLotBoy boy = new ParkingLotBoy(parkingLots);
+        Car myCar = new Car();
+
+        UUID myTicket = boy.dropOff(myCar);
+
+        assertThat(parkingLot.pickUp(myTicket), is(myCar));
+    }
+
+    @Test(expected = NoAvailableLotException.class)
+    public void should_throw_exception_when_boy_park_a_car_failure_with_two_parking_lots() throws NoAvailableLotException {
+        List<ParkingLot> parkingLots = Arrays.asList(new ParkingLot(0), new ParkingLot(0));
+        ParkingLotBoy boy = new ParkingLotBoy(parkingLots);
+        Car myCar = new Car();
+
+        boy.dropOff(myCar);
+    }
+
+    @Test
+    public void should_pick_the_origin_car_when_car_already_parked_in_parking_lots() throws NoAvailableLotException {
+        ParkingLot parkingLot = new ParkingLot(1);
+        Car myCar = new Car();
+        UUID myTicket = parkingLot.dropOff(myCar);
+        List<ParkingLot> parkingLots = Arrays.asList(new ParkingLot(0), parkingLot);
+        ParkingLotBoy boy = new ParkingLotBoy(parkingLots);
+
+        assertThat(boy.pickUp(myTicket), is(myCar));
+    }
+
+    @Test
+    public void should_not_pick_the_car_when_car_not_found_in_parking_lots() {
+        List<ParkingLot> parkingLots = Arrays.asList(new ParkingLot(1));
+        ParkingLotBoy boy = new ParkingLotBoy(parkingLots);
+
+        assertNull(boy.pickUp(UUID.randomUUID()));
+    }
+}
